51830cff99f79a2712a4d862550ce23e6cf22b7d
marcelosrg <marcelohrs2005@gmail.com>
2025-03-25 20:11:27 -0300

    feat: criando interfaces do meu repository

---
+++ src/main/java/com/marcelosg/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.marcelosg.libraryapi.repository;
+
+import com.marcelosg.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
