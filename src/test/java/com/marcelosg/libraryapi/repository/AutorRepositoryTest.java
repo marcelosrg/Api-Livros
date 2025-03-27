88cf0b7f3361e034379a27ab8828268841628354
marcelosrg <marcelohrs2005@gmail.com>
2025-03-26 21:21:44 -0300

    aprendendo a criar lista de livros do autor fazendo inserte oneToMany

--- src/test/java/com/marcelosg/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/com/marcelosg/libraryapi/repository/AutorRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.marcelosg.libraryapi.repository;
 
 import com.marcelosg.libraryapi.model.Autor;
+import com.marcelosg.libraryapi.model.Livro;
+import com.marcelosg.libraryapi.model.enuns.GeneroLivro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,10 +18,12 @@ import java.util.UUID;
 public class AutorRepositoryTest {
 
     private final AutorRepository autorRepository;
+    private final LivroRepository livroRepository;
 
     @Autowired
-    public AutorRepositoryTest(AutorRepository autorRepository) {
+    public AutorRepositoryTest(AutorRepository autorRepository, LivroRepository livroRepository) {
         this.autorRepository = autorRepository;
+        this.livroRepository = livroRepository;
     }
 
     @Test
@@ -74,5 +80,38 @@ public class AutorRepositoryTest {
         if(autor.isPresent()){
             autorRepository.deleteById(id);
         }
+    }
+
+    @Test
+    void salvarAutorComLivros(){
+        Autor autor = new Autor();
+        autor.setNome("Joe");
+        autor.setNacionalidade("brasileira");
+        autor.setDataNascimento(LocalDate.of(1941, 4, 14));
+
+
+        Livro livro = new Livro();
+        livro.setTitulo("Loucura esse Java");
+        livro.setIsbn("123456789");
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setDataPublicacao(LocalDate.of(2002,1,21));
+        livro.setPreco(BigDecimal.valueOf(54));
+        livro.setAutor(autor);
+
+        Livro livro2 = new Livro();
+        livro2.setTitulo("Loucura esse Java qqisso");
+        livro2.setIsbn("123456789");
+        livro2.setGenero(GeneroLivro.FICCAO);
+        livro2.setDataPublicacao(LocalDate.of(2002,1,21));
+        livro2.setPreco(BigDecimal.valueOf(54));
+        livro2.setAutor(autor);
+
+        autor.setLivros(new ArrayList<>());
+        autor.getLivros().add(livro);
+        autor.getLivros().add(livro2);
+
+        autorRepository.save(autor);
+
+        livroRepository.saveAll(autor.getLivros());
     }
 }
